995b28a3f38e972ab86f0587b289ddd1de5b6c23
mrdakj <mrdakj@gmail.com>
2018-06-21 02:12:29 +0200

    pca 2d visualisation

--- visualisation.py
+++ visualisation.py
@@ -1,3 +1,4 @@
+from sklearn.decomposition import PCA
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -8,6 +9,7 @@ import matplotlib.pyplot as plt
 import sys
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
 
 def isFloat(value):
   try:
@@ -23,6 +25,7 @@ def main():
     attribute_1 = 'comfort_food_reasons_coded'
     attribute_2 = 'cook'
     attribute_3 = 'eating_out'
+
 
     df = df[[attribute_1, attribute_2, attribute_3, target_attribute]]
 
@@ -52,18 +55,47 @@ def main():
     df[target_attribute] = df[target_attribute].replace(changes)
     weight = df[target_attribute].unique()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    features = [attribute_1, attribute_2, attribute_3]
+    # Separating out the features
+    x = df.loc[:, features].values
+    # Separating out the target
+    y = df.loc[:,[target_attribute]].values
+    # Standardizing the features
+    x = StandardScaler().fit_transform(x)
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(x)
+    principalDf = pd.DataFrame(data = principalComponents
+                 , columns = ['principal component 1', 'principal component 2'])
+
+    finalDf = pd.concat([principalDf, df[[target_attribute]]], axis = 1)
+
+    fig = plt.figure(figsize = (8,8))
+    ax = fig.add_subplot(1,1,1) 
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_title('2 component PCA', fontsize = 20)
+    targets = [0,1,2]
+    colors = ['r', 'g', 'b']
+    for target, color in zip(targets,colors):
+        indicesToKeep = finalDf[target_attribute] == target
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+                   , finalDf.loc[indicesToKeep, 'principal component 2']
+                   , c = color
+                   , s = 50)
+    ax.legend(targets)
+    ax.grid()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
 
     
-    colors = ['green', 'blue','red']
-    for (v, color) in zip(weight, colors):
-        subsamples = df.loc[df[target_attribute] == v]
-        ax.scatter(subsamples[attribute_1], subsamples[attribute_2], subsamples[attribute_3],color=color, s=70, alpha=0.3)
+    # colors = ['green', 'blue','red']
+    # for (v, color) in zip(weight, colors):
+    #     subsamples = df.loc[df[target_attribute] == v]
+    #     ax.scatter(subsamples[attribute_1], subsamples[attribute_2], subsamples[attribute_3],color=color, s=70, alpha=0.3)
 
-    ax.set_xlabel('comfort_food_reasons_coded')
-    ax.set_ylabel('cook')
-    ax.set_zlabel('eating_out')
+    # ax.set_xlabel('comfort_food_reasons_coded')
+    # ax.set_ylabel('cook')
+    # ax.set_zlabel('eating_out')
     plt.show()
 
 if __name__ == "__main__":
